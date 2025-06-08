93f3f6c83bb58983d2c94d73667edac045d65f66
Henry Jimenez <henry.sistemas.jimenez@gmail.com>
2025-06-08 13:15:13 -0500

    add public companies view

--- backend/companies/urls/v1.py
+++ backend/companies/urls/v1.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from companies.views.v1 import CompanyViewSet
+from companies.views.v1 import CompanyPublicViewSet, CompanyAdminViewSet
 
 router_v1_companies = DefaultRouter()
-router_v1_companies.register(r'companies', CompanyViewSet)
+router_v1_companies.register(r'companies', CompanyPublicViewSet)
+router_v1_companies.register(r'my-companies', CompanyAdminViewSet, basename='companies-admin')
 
 app_name = 'companies'
 
--- backend/companies/views/v1.py
+++ backend/companies/views/v1.py
@@ -7,36 +7,37 @@ from ..serializers.v1 import CompanySerializer
 from ..permissions.v1 import IsCompanyAdministrator
 from ..filters.v1 import CompanyFilter
 
-class CompanyViewSet(viewsets.ModelViewSet):
+
+class CompanyPublicViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    Permite manejar las operaciones CRUD de la entidad Company.
-    - Listar todas las compañías (Público)
-    - Crear, actualizar y eliminar compañías (Requiere autenticación)
-    """    
+    Vista pública: lista todas las compañías. Permite solo GET.(Vista para usuario no autenticado)
+    """
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+    permission_classes = [permissions.AllowAny]
 
-    # Configuración de filtros
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_class = CompanyFilter
-
-    # Para búsqueda integral por el parámetro 'search'
     search_fields = ['nit', 'name']
-
-    # Configuración de ordenamiento
-    ordering_fields = ['nit','name','created_at']
+    ordering_fields = ['nit', 'name', 'created_at']
     ordering = ['-created_at']
 
-    def get_permissions(self):
-        #Permisos para creación
-        if self.action in ['create']:
-            return [permissions.IsAuthenticated()]
-        #Permisos para actualización y eliminación
-        elif self.action in ['update', 'partial_update', 'destroy']:
-            return [permissions.IsAuthenticated(), IsCompanyAdministrator()]
-        #Permisos de Lectura
-        return [permissions.AllowAny()]
+class CompanyAdminViewSet(viewsets.ModelViewSet):
+    """
+    Vista privada para administración:
+    Solo permite ver y gestionar empresas donde el usuario es administrador.
+    """
+    serializer_class = CompanySerializer
+    permission_classes = [permissions.IsAuthenticated, IsCompanyAdministrator]
 
-    # Asigna automáticamente el usuario logueado al crear
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = CompanyFilter
+    search_fields = ['nit', 'name']
+    ordering_fields = ['nit', 'name', 'created_at']
+    ordering = ['-created_at']
+
+    def get_queryset(self):
+        return Company.objects.filter(administrator=self.request.user)
+
     def perform_create(self, serializer):
         serializer.save(administrator=self.request.user)
